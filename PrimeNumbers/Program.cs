281ec798c23ad51f01bd0a0a7ca41fdb5d5cb0d0
Bharath <kammalabharath96@gmail.com>
2026-02-27 11:06:25 +0530

    Intaial commit

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number:");
+int inputnum = int.Parse(Console.ReadLine());
+for (int i =2;i<=inputnum;i++)
+{
+    int count=0;
+    for (int j=2;j<i;j++)
+    {
+        if (i%j==0)
+        {
+           count =count+1;
+            break;
+        }
+    }
+    if (count==0)
+    {
+        Console.WriteLine(i);
+    }
+}
